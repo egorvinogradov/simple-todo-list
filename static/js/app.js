48452f2aba5eabebb0fbdce1309be62de1c9e24b
Egor Vinogradov <egor@ostrovok.ru>
2012-09-13 14:53:32 +0400

    updated design; changed key events

--- static/js/app.js
+++ static/js/app.js
@@ -4,17 +4,78 @@ App = function(config){
     this.config = config;
 };
 
-App.prototype.init = function(){
-    this.els = {};
-    this.els.container = $(this.config.selectors.container);
+App.prototype.keyConfig = {
+    addRowAboveToSelection: {
+        condition: function(event){
+            return event.shiftKey && event.which === 38;
+        },
+        behaviour: function(){
+            this.moveSelection({
+                up: true,
+                multiple: true
+            });
+        }
+    },
+    addRowBelowToSelection: {
+        condition: function(event){
+            return event.shiftKey && event.which === 40;
+        },
+        behaviour: function(){
+            this.moveSelection({
+                down: true,
+                multiple: true
+            });
+        }
+    },
+    moveSelectionUp: {
+        condition: function(event){
+            return !event.shiftKey && event.which === 38;
+        },
+        behaviour: function(){
+            this.moveSelection({
+                up: true
+            });
+        }
+    },
+    moveSelectionDown: {
+        condition: function(event){
+            return !event.shiftKey && event.which === 40;
+        },
+        behaviour: function(){
+            this.moveSelection({
+                down: true
+            });
+        }
+    },
+    removeTask: {
+        condition: function(event){
+            var target = $(event.target);
+            return event.which === 8 && target.is(this.config.selectors.text) && !this.trimTags(target.html());
+        },
+        behaviour: function(event){
+            var task = $(event.target).parents(this.config.selectors.listItem);
+            this.removeTask(task);
+        }
+    },
+    resolveTask: {
+        condition: function(event){
+            return false; // todo: fix
+        },
+        behaviour: function(event){
+            var task = $(event.target).parents(this.config.selectors.listItem);
+            this.resolveTask(task);
+        }
+    }
+};
 
+App.prototype.init = function(){
+    this.container = $(this.config.selectors.container);
     this.model = new Model({
         url: this.config.url
     });
     this.model.fetch({
         success: $.proxy(function(){
-            console.log('model fetch success', this.model);
-            this.render(this.model.toArray(), this.els.container, $.proxy(this.bindEvents, this));
+            this.render(this.model.toArray(), this.container, $.proxy(this.bindEvents, this));
         }, this),
         error: $.proxy(function(){
             console.error('Can\'t fetch model');
@@ -129,7 +190,6 @@ App.prototype.sortTasks = function(data){
 };
 
 App.prototype.setModel = function(id, params){
-    console.log('^^^ set model', params);
     if ( this.is_save_timer ) {
         clearTimeout(this.save_timer);
     }
@@ -141,7 +201,6 @@ App.prototype.setModel = function(id, params){
         this.is_save_timer = false;
         this.model.save({
             success: $.proxy(function(){
-                console.log('^^^ saved');
             }, this),
             error:$.proxy(function(data){
                 console.error('Can\'t save model', data);
@@ -150,39 +209,141 @@ App.prototype.setModel = function(id, params){
     }, this), 1000);
 };
 
-App.prototype.bindEvents = function(tasks){
-    tasks = tasks && tasks.length
-        ? tasks
-        : $(this.config.selectors.listItem);
-    tasks.eq(0)
-        .find(this.config.selectors.wrapper)
-        .first()
-        .addClass(this.config.classes.selected);
-    tasks.each($.proxy(function(i, task){
-        this.bindTextEditingEvents( $(this.config.selectors.text, task) );
-        this.bindCheckboxEvents( $(this.config.selectors.checkbox, task) );
-        this.bindAddButtonEvents( $(this.config.selectors.add, task) );
-    }, this));
-    this.bindKeyEvents(this.keys);
+App.prototype.getTasks = function(ids){
+    var tasks = $(this.config.selectors.listItem, this.container),
+        result = $();
+    if ( !ids ) {
+        return tasks;
+    }
+    ids = ids instanceof Array ? ids : [ids];
+    for ( var i = 0, l = ids.length; i < l; i++ ) {
+        var task = tasks.filter('[data-id=\'' + ids[i] + '\']');
+        result = result.add(task);
+    }
+    return result;
 };
 
-App.prototype.bindTextEditingEvents = function(els){
+App.prototype.selectTasks = function(tasks){
+    tasks = tasks instanceof jQuery
+        ? tasks
+        : this.getTasks(tasks);
+    tasks.addClass(this.config.classes.selected);
+    return tasks;
+};
+
+App.prototype.getSelectedTasks = function(){
+    return this.getTasks().filter('.' + this.config.classes.selected);
+};
+
+App.prototype.resetSelection = function(){
+    return this.getSelectedTasks().removeClass(this.config.classes.selected);
+};
+
+App.prototype.moveSelection = function(params){
+    
+//        var tasks = this.getTaskElements(),
+//        selection = this.getSelectedTasks(),
+//        current = this.getTaskIndex( selection.last() ),
+//        next = tasks.eq( params.up ? current - 1 : current + 1 );
+//
+//    if ( params.multiple && !next.length ) {
+//        return;
+//    }
+//    if ( !params.multiple ) {
+//        selection
+//            .children(this.config.selectors.wrapper)
+//            .removeClass(this.config.classes.selected);
+//    }
+//    next = next.length
+//        ? next
+//        : tasks.filter( params.up ? ':last' : ':first' );
+//    next
+//        .children(this.config.selectors.wrapper)
+//        .toggleClass(this.config.classes.selected);
+
+    console.log('move selection ' + ( params.up ? 'up' : 'down' ) + ( params.multiple ? ', multiple' : '' ), current);
+};
+
+App.prototype.removeTask = function(task){
+    var taskEl,
+        taskId;
+    if ( task instanceof jQuery ) {
+        taskEl = task;
+        taskId = +taskEl.data('id');
+    }
+    else {
+        taskId = task;
+        taskEl = this.getTasks(taskId);
+    }
+    this.setModel(taskId, null);
+    taskEl.remove();
+};
+
+App.prototype.resolveTask = function(task){
+    var taskEl,
+        taskId;
+    if ( task instanceof jQuery ) {
+        taskEl = task;
+        taskId = +taskEl.data('id');
+    }
+    else {
+        taskId = task;
+        taskEl = this.getTasks(taskId);
+    }
+    this.setModel(taskId, {
+        done: true
+    });
+    // todo: update DOM when model.change fires
+};
+
+App.prototype.bindEvents = function(){
+    var tasks = this.getTasks();
+    this.selectTasks(tasks.first());
+    this.bindKeyEvents(this.keyConfig);
+    this.bindTextEditingEvents(tasks);
+};
+
+App.prototype.bindKeyEvents = function(config){
+    var frame = $(window);
+    frame._on('keydown', function(event){
+        console.log('key down', event.currentTarget, event.target, event.which, event.shiftKey ? 'shift' : '', event.ctrlKey ? 'ctrl' : '');
+        for ( var action in config ) {
+            if ( config[action].condition.call(this, event) ) {
+                config[action].behaviour.call(this, event);
+            }
+        }
+    }, this);
+    frame._on('click', function(event){
+        var target = $(event.target),
+            task = target.is(this.config.selectors.wrapper)
+                ? target
+                : target.parents(this.config.selectors.wrapper);
+        this.resetSelection();
+        if ( task.length ) {
+            this.selectTasks(task.parent());
+        }
+    }, this);
+};
+
+App.prototype.bindTextEditingEvents = function(tasks){
+
+    var els = tasks.find(this.config.selectors.text);
 
     els.each(function(i, element){
         element.contentEditable = true;
     });
 
     els._on('focus', function(event){
-        var element = $(event.currentTarget);
+        var element = $(event.currentTarget),
+            task = element.parents(this.config.selectors.listItem).first();
         element.data({
-            //before: $.trim(element.html())
             before: this.trimTags(element.html())
         });
+        this.selectTasks(task);
     }, this);
 
     els._on('blur keyup paste', function(event){
         var element = $(event.currentTarget),
-            //text = $.trim(element.html()),
             text = this.trimTags(element.html()),
             task,
             id;
@@ -195,205 +356,4 @@ App.prototype.bindTextEditingEvents = function(els){
             });
         }
     }, this);
-};
-
-App.prototype.bindCheckboxEvents = function(els){
-    els._on('change', function(event){
-        var checkbox = $(event.currentTarget),
-            task = checkbox.parents(this.config.selectors.listItem),
-            id = +task.data('id'),
-            checked = !!checkbox.filter(':checked').length;
-        if ( checked ) {
-            this.setModel(id, {
-                done: checked,
-                finish: new Date().toISOString()
-            });
-        }
-        else {
-            this.setModel(id, {
-                done: checked,
-                finish: null
-            });
-        }
-    }, this);
-};
-
-App.prototype.bindAddButtonEvents = function(els){
-
-    els._on('click', function(event){
-        var els = {},
-            data = {},
-            newTask = {};
-
-        els.task = $(event.currentTarget).parents(this.config.selectors.listItem);
-        els.parent = els.task.parents(this.config.selectors.listItem);
-        data.id = +els.task.data('id');
-        data.parent = +els.parent.data('id') || 0;
-        newTask.id = +new Date();
-        newTask.data = {
-            id: newTask.id,
-            parent: data.parent,
-            text: this.config.newTaskText,
-            start: new Date(newTask.id).toISOString(),
-            finish: null,
-            order: null,
-            done: false
-        };
-        newTask.html = _.template(this.config.templates.listItem, _.extend(newTask.data, { tasksHtml: '' }));
-        newTask.element = $(newTask.html);
-
-        els.task.after(newTask.element);
-        this.setModel(newTask.id, newTask.data);
-        this.bindEvents(newTask.element);
-        newTask.element
-            .find(this.config.selectors.text)
-            .focus();
-    }, this);
-};
-
-App.prototype.keys = {
-    selectRowAbove: {
-        condition: function(event){
-            return event.shiftKey && event.which === 38;
-        },
-        behaviour: function(){
-            this.moveSelection({
-                up: true,
-                multiple: true
-            });
-        }
-    },
-    selectRowBelow: {
-        condition: function(event){
-            return event.shiftKey && event.which === 40;
-        },
-        behaviour: function(){
-            this.moveSelection({
-                down: true,
-                multiple: true
-            });
-        }
-    },
-    moveSelectionUp: {
-        condition: function(event){
-            return !event.shiftKey && event.which === 38;
-        },
-        behaviour: function(){
-            this.moveSelection({
-                up: true
-            });
-        }
-    },
-    moveSelectionDown: {
-        condition: function(event){
-            return !event.shiftKey && event.which === 40;
-        },
-        behaviour: function(){
-            this.moveSelection({
-                down: true
-            });
-        }
-    },
-    removeTask: {
-        condition: function(event){
-            var target = $(event.target);
-            return event.which === 8 && target.is(this.config.selectors.text) && !this.trimTags(target.html());
-        },
-        behaviour: function(event){
-            var id = +$(event.target)
-                .parents(this.config.selectors.listItem)
-                .data('id');
-            this.removeTask(id);
-            console.log('REMOVE', id, event.target);
-        }
-    },
-    resolveTask: {
-        condition: function(event){
-            return ''
-        },
-        behaviour: function(event){
-
-        }
-    }
-};
-
-App.prototype.bindKeyEvents = function(config){
-
-    var _window = $(window);
-
-    _window._on('keydown', function(event){
-        console.log(
-            'key down',
-            event.currentTarget,
-            event.target,
-            event.which,
-            event.shiftKey ? 'shift' : '',
-            event.ctrlKey ? 'ctrl' : ''
-        );
-        for ( var action in config ) {
-            if ( config[action].condition.call(this, event) ) {
-                config[action].behaviour.call(this, event);
-            }
-        }
-    }, this);
-
-    _window._on('click', function(){
-        this.getSelectedTasks()
-            .find(this.config.selectors.wrapper)
-            .removeClass(this.config.classes.selected);
-    }, this);
-};
-
-App.prototype.getTaskElements = function(sibling){
-    var parent = sibling && sibling.parent().length
-        ? sibling.parent()
-        : document;
-    return $(this.config.selectors.listItem, parent);
-};
-
-App.prototype.getSelectedTasks = function(){
-    return $(this.config.selectors.wrapper)
-        .filter('.' + this.config.classes.selected)
-        .parent();
-};
-
-App.prototype.getTaskIndex = function(task){
-    var tasks = this.getTaskElements();
-    for ( var i = 0, l = tasks.length; i < l; i++ ) {
-        if ( tasks.eq(i).is(task) ) {
-            return i;
-        }
-    }
-};
-
-App.prototype.moveSelection = function(params){
-
-    var tasks = this.getTaskElements(),
-        selection = this.getSelectedTasks(),
-        current = this.getTaskIndex( selection.last() ),
-        next = tasks.eq( params.up ? current - 1 : current + 1 );
-
-    if ( params.multiple && !next.length ) {
-        return;
-    }
-    if ( !params.multiple ) {
-        selection
-            .children(this.config.selectors.wrapper)
-            .removeClass(this.config.classes.selected);
-    }
-    next = next.length
-        ? next
-        : tasks.filter( params.up ? ':last' : ':first' );
-    next
-        .children(this.config.selectors.wrapper)
-        .toggleClass(this.config.classes.selected);
-
-    console.log('move selection ' + ( params.up ? 'up' : 'down' ) + ( params.multiple ? ', multiple' : '' ), current);
-};
-
-App.prototype.removeTask = function(id){
-    $(this.config.selectors.listItem)
-        .filter('[data-id="' + id + '"]')
-        .remove();
-    this.setModel(id, null);
 };
