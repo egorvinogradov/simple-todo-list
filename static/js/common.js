592a6f8938953f31b861dcab66e53ff686599c1f
Egor Vinogradov <egor@ostrovok.ru>
2012-09-27 17:22:09 +0400

    moved mock to separate file

--- static/js/common.js
+++ static/js/common.js
@@ -31,45 +31,4 @@ var Config = {
     newTaskText: 'Новая задача'
 };
 
-var mock = [
-    {
-        id: 1,
-        text: 'wrrtj6uki8k',
-        start: '2012-09-11T13:41:46.020Z',
-        finish: null,
-        done: false,
-        parent: 0,
-        order: 1
-    },
-    {
-        id: 2,
-        text: 'xvdnu7h54g4c',
-        start: '2012-09-11T13:41:47.020Z',
-        finish: null,
-        done: true,
-        parent: 1,
-        order: 1
-    },
-    {
-        id: 3,
-        text: '78967u4t34f45gb5h',
-        start: '2012-09-11T13:41:48.020Z',
-        finish: null,
-        done: false,
-        parent: 1,
-        order: 2
-    },
-    {
-        id: 4,
-        text: 'dghntymkuyj6rf',
-        start: '2012-09-11T13:41:49.020Z',
-        finish: null,
-        done: false,
-        parent: 0,
-        order: null
-    }
-];
-
-//localStorage.setItem('tasks', JSON.stringify(mock));
-
 var Todo = new App(Config);
