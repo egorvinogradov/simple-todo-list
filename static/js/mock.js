932386722f97fdac16bebdaf96c26c279d2d0306
Egor Vinogradov <egor@ostrovok.ru>
2012-09-27 17:22:33 +0400

    moved mock to separate file

---
+++ static/js/mock.js
@@ -0,0 +1,120 @@
+var mock1 = [
+    {
+        id: 1,
+        text: 'Настроить клавиши в проигрывателе',
+        start: '2012-09-11T13:41:46.020Z',
+        finish: null,
+        done: false,
+        parent: 0,
+        order: 1
+    },
+    {
+        id: 2,
+        text: 'Поставить в проигрыватель плагин last.fm',
+        start: '2012-09-11T13:41:47.020Z',
+        finish: null,
+        done: true,
+        parent: 1,
+        order: 1
+    },
+    {
+        id: 3,
+        text: 'Перевести деньги на карту',
+        start: '2012-09-11T13:41:48.020Z',
+        finish: null,
+        done: false,
+        parent: 1,
+        order: 2
+    },
+    {
+        id: 4,
+        text: 'Указать новые данные карты для оплаты dropbox и last.fm',
+        start: '2012-09-11T13:41:49.020Z',
+        finish: null,
+        done: false,
+        parent: 0,
+        order: null
+    },
+    {
+        id: 5,
+        text: 'Проверить оплату аккаунта в github и basecamp',
+        start: '2012-09-11T13:42:11.020Z',
+        finish: null,
+        done: false,
+        parent: 0,
+        order: null
+    },
+    {
+        id: 6,
+        text: 'Скачать новый альбом DSHS',
+        start: '2012-09-11T13:42:15.020Z',
+        finish: null,
+        done: false,
+        parent: 0,
+        order: null
+    }
+];
+
+
+var mock2 = [
+    {
+        id: 1,
+        text: 'Настроить клавиши в проигрывателе',
+        start: '2012-09-11T13:41:46.020Z',
+        finish: null,
+        done: false,
+        parent: 0,
+        order: 1
+    },
+    {
+        id: 2,
+        text: 'Поставить в проигрыватель плагин last.fm',
+        start: '2012-09-11T13:41:47.020Z',
+        finish: null,
+        done: true,
+        parent: 0,
+        order: 2
+    },
+    {
+        id: 3,
+        text: 'Перевести деньги на карту',
+        start: '2012-09-11T13:41:48.020Z',
+        finish: null,
+        done: false,
+        parent: 0,
+        order: null
+    },
+    {
+        id: 4,
+        text: 'Указать новые данные карты для оплаты dropbox и last.fm',
+        start: '2012-09-11T13:41:49.020Z',
+        finish: null,
+        done: false,
+        parent: 0,
+        order: null
+    },
+    {
+        id: 5,
+        text: 'Проверить оплату аккаунта в github и basecamp',
+        start: '2012-09-11T13:42:11.020Z',
+        finish: null,
+        done: false,
+        parent: 0,
+        order: null
+    },
+    {
+        id: 6,
+        text: 'Скачать новый альбом DSHS',
+        start: '2012-09-11T13:42:15.020Z',
+        finish: null,
+        done: false,
+        parent: 0,
+        order: null
+    }
+];
+
+/*
+
+localStorage.setItem('tasks', JSON.stringify(mock2));
+
+ */
